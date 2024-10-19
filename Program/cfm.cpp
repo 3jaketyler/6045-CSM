5ed51e01edface8586eb0fc78ed723eb7ed45ee7
NoSlurp <143546645+NoSlurp@users.noreply.github.com>
2024-10-19 17:53:45 -0400

    Update cfm.cpp

--- Program/cfm.cpp
+++ Program/cfm.cpp
@@ -10,21 +10,22 @@ using namespace std;
 
 void welcomeMsg();                          // prints welcome message
 vector<Person> readData(string filename);   // get data from file
-void settleDebts(vector<Person> people);                   // calculates who should pay who
+void settleDebts(vector<Person>& people);   // calculates who should pay who
+int findName(const vector<Person>& people, const string& name);  // helper function to find a person by name
 
 int main() {
-    
-
     welcomeMsg();
 
     string filename;
     cout << "\nEnter the name of your data file: ";
     cin >> filename;
 
-    vector<Person> people;
-    while (people.empty()) {      // prompts user for data 
-        people = readData(filename);
-    } 
+    vector<Person> people = readData(filename);
+
+    if (people.empty()) {
+        cout << "Failed to load data or no valid data found in file. Exiting program.\n";
+        return 1;  // Exit the program if no data is found
+    }
 
     cout << "Calculating who should pay whom..." << endl;
     settleDebts(people);
@@ -33,11 +34,11 @@ int main() {
 }
 
 void welcomeMsg() {     // welcome message
-    cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++\n" 
-         << "+                                                  +\n"
-         << "+        Welcome to the Cash Flow Minimizer        +\n"
-         << "+                                                  +\n"
-         << "++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+    cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+        << "+                                                  +\n"
+        << "+        Welcome to the Cash Flow Minimizer        +\n"
+        << "+                                                  +\n"
+        << "++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
 }
 
 vector<Person> readData(string filename) {      // reads data from file
@@ -56,8 +57,6 @@ vector<Person> readData(string filename) {      // reads data from file
         cout << "Invalid input format on line 1. Please try again.\n";
         f.clear();               // Clear the error flags
         f.ignore(numeric_limits<streamsize>::max(), '\n');
-        people.clear();
-        people.resize(0);
         return people;
     }
 
@@ -69,47 +68,60 @@ vector<Person> readData(string filename) {      // reads data from file
             cout << "Invalid input format on line " << i + 2 << ". Please try again.\n";
             f.clear();               // Clear the error flags
             f.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            people.clear();
-            people.resize(0);
             return people;
         }
 
-        int dIndex = findName(people, debtor);      // searches vector for the debtors index
-        int cIndex = findName(people, creditor);    // searches vector for the creditors index
+        int dIndex = findName(people, debtor);      // searches vector for the debtor's index
+        int cIndex = findName(people, creditor);    // searches vector for the creditor's index
 
         if (dIndex == -1) {         // adds new person to list
             Person newPerson(debtor, debt, 0);
             people.push_back(newPerson);
-        } else {                    // modifies debt parameter for new debt
+        }
+        else {                    // modifies debt parameter for new debt
             people[dIndex].addDebt(debt);
         }
 
         if (cIndex == -1) {         // adds new person to list
             Person newPerson(creditor, 0, debt);
             people.push_back(newPerson);
-        } else {                    // modifies credit parameter for new credit
+        }
+        else {                    // modifies credit parameter for new credit
             people[cIndex].addCredit(debt);
         }
     }
+    return people;
 }
 
-void settleDebts(vector<Person> people) {
-    // cancels out each persons debt and credit with themselves
+int findName(const vector<Person>& people, const string& name) {
+    for (size_t i = 0; i < people.size(); ++i) {  // Changed int to size_t to match the type of people.size()
+        if (people[i].getName() == name) {
+            return i;  // return the index of the person if found
+        }
+    }
+    return -1;  // return -1 if the person is not found
+}
+
+void settleDebts(vector<Person>& people) {
+    // cancels out each person's debt and credit with themselves
     cout << "\nCancelling out everyone's debt and credit:\n";
-    int s = people.size();
-    for (int i = 0; i < s; i++) {
-        if (people[i].getCredit() < people[i].getDebt()) {
-            people[i].subDebt(people[i].getCredit());
-            cout << people[i].getName() << "'s credit cancels out. They will not get paid. They still owe $" << people[i].getDebt() << ".\n";
-        } else if (people[i].getDebt() < people[i].getCredit()) {
-            people[i].subCredit(people[i].getDebt());
-            cout << people[i].getName() << "'s debt cancels out. They will not pay anyone. They still are owed $" << people[i].getCredit() << ".\n";
-        } else {
-            cout << people[i].getName() << "'s debt and credit cancels out. They will not pay anyone or get paid\n";
-            people.erase(people.begin() + i);
+    size_t s = people.size();  // Correct type
+
+    for (size_t i = s; i > 0; --i) {  // Reverse loop to erase elements safely
+        size_t idx = i - 1;  // Correct index when using size_t in reverse loop
+        if (people[idx].getCredit() < people[idx].getDebt()) {
+            people[idx].subDebt(people[idx].getCredit());
+            cout << people[idx].getName() << "'s credit cancels out. They will not get paid. They still owe $" << people[idx].getDebt() << ".\n";
+        }
+        else if (people[idx].getDebt() < people[idx].getCredit()) {
+            people[idx].subCredit(people[idx].getDebt());
+            cout << people[idx].getName() << "'s debt cancels out. They will not pay anyone. They still are owed $" << people[idx].getCredit() << ".\n";
+        }
+        else {
+            cout << people[idx].getName() << "'s debt and credit cancels out. They will not pay anyone or get paid.\n";
+            people.erase(people.begin() + idx);  // Safely erase while iterating backward
         }
     }
 
-    // iterating through list to determine who will pay who
-    
+    // Additional logic for settling remaining debts/credits can be implemented here
 }
